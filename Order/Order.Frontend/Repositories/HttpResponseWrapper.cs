edbbd91152cabb398d46eb42804a87e0bee645c2
NRRozborski <norbert.rozborski@educa.madrid.org>
2024-04-03 15:51:29 +0200

    Begin repository pattern.

---
+++ Order/Order.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace Order.Frontend.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+            var statusCode = HttpResponseMessage.StatusCode;
+            switch (statusCode)
+            {
+                case HttpStatusCode.NotFound:
+                    return "Recurso no encontrado.";
+
+                case HttpStatusCode.Unauthorized:
+                    return "Acceso no autorizado.";
+
+                case HttpStatusCode.Forbidden:
+                    return "Acceso denegado.";
+
+                case HttpStatusCode.BadRequest:
+                    return await HttpResponseMessage.Content.ReadAsStringAsync();
+
+                default:
+                    return "Ha ocurrido un error inesperado.";
+            }
+        }
+    }
+}
