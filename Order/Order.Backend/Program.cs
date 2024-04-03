23548178036c27f5dbef5f737b99a530220930f8
NRRozborski <norbert.rozborski@educa.madrid.org>
2024-04-03 13:31:56 +0200

    Add basic countries API controller

--- Order/Order.Backend/Program.cs
+++ Order/Order.Backend/Program.cs
@@ -3,13 +3,18 @@ using Order.Backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
 
 if (app.Environment.IsDevelopment())
 {
